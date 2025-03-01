2966fc65bebc3122834d359def566186125b710e
Om Berde <omberde0@gmail.com>
2025-03-01 20:44:01 +0530

    Casual

--- obj/Debug/net8.0/Kick X.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kick X.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kick X")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd56eefa743e7c4f7f342690fed0b3b377c48cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kick X")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kick X")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
